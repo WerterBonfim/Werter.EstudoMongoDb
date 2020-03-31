2e5533160fcff661918a21cb419742eb2331fe86
werter <werter@hotmail.com.br>
2020-03-31 12:47:20 -0300

    inicial

---
+++ Werter.EstudoMongoDb.Infra/SuporteColecao.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace Werter.EstudoMongoDb.Infra
+{
+    public sealed class SuporteColecao<T> : SuporteDb
+    {
+        public IMongoCollection<T> Colecao { get; private set; }
+
+        public SuporteColecao(string stringDeConexao, string nomeBanco) : base(stringDeConexao)
+        {
+            DefinirBanco(nomeBanco);            
+        }
+
+        public SuporteColecao<T> DefinirColecao(string nome)
+        {
+            Colecao = Banco.GetCollection<T>(nome);
+            return this;
+        }
+    }
+}
---
+++ Werter.EstudoMongoDb.Infra/SuporteDb.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+
+namespace Werter.EstudoMongoDb.Infra
+{
+    public abstract class SuporteDb
+    {
+        protected IMongoClient Cliente { get; private set; }
+        protected IMongoDatabase Banco { get; private set; }
+        
+        public SuporteDb(string stringDeConexao)
+        {
+            Cliente = new MongoClient(stringDeConexao);            
+        }
+
+        public SuporteDb DefinirBanco(string nome)
+        {
+            Banco = Cliente.GetDatabase(nome);
+            return this;
+        }
+        
+    }
+}
